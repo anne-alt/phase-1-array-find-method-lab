405fb2630cc63a1fc4ef793880947de53f8ed3fe
Anne Ogongo <anneogongo98@gmail.com>
2022-08-21 19:19:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+const win =record.find( item => item.result === "W") 
+    if (!!win === true) {
+   console.log (win.year);   
+   return win.year;
+    }
+}
